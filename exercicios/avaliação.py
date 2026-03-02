c3e0065cc12be0f5b617e7d066bcf07dd6d08bbc
brudeck <isabella.brudeck.machado@escola.pr.gov.br>
2026-03-02 08:12:30 -0300

    .

---
+++ exercicios/avaliação.py
@@ -0,0 +1,4 @@
+dia=input ('nossa atividade esta sendo realizada no dia')
+mes=input ('nossa atividade esta sendo realizada no mes')
+ano=input ('nossa atividade esta sendo realizada no ano')
+print('dia:{} mês:{} ano:{}' .format (dia, mes, ano))
