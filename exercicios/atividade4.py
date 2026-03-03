c5be811686b21f4c1383f2868bbfc578d5d46eda
brudeck <isabella.brudeck.machado@escola.pr.gov.br>
2026-03-03 09:50:16 -0300

    ..

--- exercicios/atividade4.py
+++ exercicios/atividade4.py
@@ -3,3 +3,7 @@
 #float: é para números decimais 
 #bool: armazenar valores de verdadeiro e falso
 #str: armazena textos
+
+num1 = int (input ("qual o primeiro numero"))
+num2 = int (input ("qual o segundo numero"))
+print (num1 + num2)
---
+++ exercicios/atividades5.py
@@ -0,0 +1,4 @@
+num1 = int (input ("digite seu primeiro número"))
+num2 = int (input ("digite seu segundo número"))
+soma = num1 + num2
+print ("a soma entre {} e {} é {}" .format (num1, num2, soma))
