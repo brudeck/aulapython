9b50298b394fab6d431fd7714d103d4ad984209b
brudeck <isabella.brudeck.machado@escola.pr.gov.br>
2026-03-09 11:47:10 -0300

    ..

--- exercicios/conteúdo/cont2.py
+++ exercicios/conteúdo/cont2.py
@@ -22,3 +22,13 @@ print(5==(2.5*2))
 # potencição
 # multiplicação e divisão 
 # soma e subtração
+
+print(5+3*2)
+print(3*5+4**2)
+print(3*(5+4)**2)
+
+# pow (x,y) : pow = potenciação; x = numero de cima; y = numero de baixo
+
+print(pow(3,4))
+print(27**(1/3))
+print('oi'* 5)
