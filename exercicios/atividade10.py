4bd3bb429c242f5ce5946828cc3677aa14583527
brudeck <isabella.brudeck.machado@escola.pr.gov.br>
2026-03-11 10:56:22 -0300

    ..

---
+++ exercicios/atividade10.py
@@ -0,0 +1,4 @@
+num1 = float (input('digite seu valor'))
+cem=num1*100
+mili=num1*1000
+print('o valor em centimetros é {} e o valor em milimetros é {}'.format(num1,mili, cem))
---
+++ exercicios/atividade11.py
@@ -0,0 +1,3 @@
+num1 = int (input('digite um número'))
+num2 = num1*1, num1*2, num1*3, num1*4, num1*5, num1*6, num1*7, num1*8, num1*9, num1*10
+print('a tabuada do {} é {}'.format(num1,num2))
