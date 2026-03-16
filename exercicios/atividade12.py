8ca044cd2595be6cfd7a45e3626dc579d50f4b8e
brudeck <isabella.brudeck.machado@escola.pr.gov.br>
2026-03-16 08:16:57 -0300

    ..

---
+++ exercicios/atividade12.py
@@ -0,0 +1,4 @@
+carte=float(input('quanto dinheiro na carteira voce tem?'))
+dolar=5.38
+conve=carte*dolar
+print('voce tem {} reais em dinheiro, convertendo em dolares voce tem {}'.format(carte,conve))
