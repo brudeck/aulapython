ee4ee11f6c1bdc98f79644d9d53d0129c29ceb2e
brudeck <isabella.brudeck.machado@escola.pr.gov.br>
2026-04-08 11:50:03 -0300

    ..

--- exercicios/conteúdo/cont7.py
+++ exercicios/conteúdo/cont7.py
@@ -1,4 +1,4 @@
-frase = ('curso de análise e desenvolvimento de sistemas')
+frase = (' curso de análise e desenvolvimento de sistemas ')
 print(frase[3]) #isso mostra o espaço da memória
 print(frase[3:46]) #intervalo de caracteres menos o último
 print(frase[3:45:2]) #pula de dois em dois 
@@ -12,3 +12,13 @@ print(frase.count('a',3,5)) #procura um caracter específico dentro de um interv
 print(frase.find('urso')) #mostra apartir de qual índice aparece a pesquisa 
 print(frase.find('android')) #o resultado -1 indica que não tem essa sequencia na variavel
 print('abacate'in frase) #o in verifica se aquele conjunto de string está na variavel
+print(frase.replace('desenvolvimento','programação')) #troca a palavra 
+print(frase.upper()) #deixa em maiusculo
+print(frase.lower()) #deixa em minusculo
+print(frase.capitalize()) #deixa a primeira letra da primeira palavra em maiusculo
+print(frase.title()) #deixa todas as primeiras letras em maiusculo 
+print(frase.strip()) #tira o espaço em branco do começo e do final 
+
+print(frase.split()) #transforma o valor da variavel em lista 
+print('$'.join(frase)) #une um simbolo em cada parte do split
+
