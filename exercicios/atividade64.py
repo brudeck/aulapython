59821ff328aeb78e88f485c9e4704510c1ef49a0
isabella <isabella.brudeck.machado@escola.pr.gov.br>
2026-08-11 09:03:40 -0300

    ..

---
+++ exercicios/atividade64.py
@@ -0,0 +1,22 @@
+extenso = (
+    "zero",
+    "um",
+    "dois",
+    "três",
+    "quatro",
+    "cinco",
+    "seis",
+    "sete",
+    "oito",
+    "nove",
+    "dez",
+)
+
+while True:
+    num = int(input("Digite um número entre 0 e 10: "))
+
+    if 0 <= num <= 10:
+        break
+    print("Tente novamente. ", end="")
+
+print(f"Você digitou o número {extenso[num]}.")
--- exercicios/conteúdo/cont13.py
+++ exercicios/conteúdo/cont13.py
@@ -1,8 +1,33 @@
-lanche = ("hamburguer","suco","pizza","pudim")
-print(lanche)
-print(lanche [1])
-print(lanche [-2])
-print(lanche [0:4])
-print(lanche [2:])
-print(lanche [:2])
-print(lanche [-2::-1])
+#lanche = ("hamburguer","suco","pizza","pudim")
+#print(lanche)
+#print(lanche [1])
+#print(lanche [-2])
+#print(lanche [0:4])
+#print(lanche [2:])
+#print(lanche [:2])
+#print(lanche [-2::-1])
+
+# lanche = ('hamburguer','suco','pizza','pudim')
+# for comida in lanche:
+    # print(f'eu vou comer {comida}')
+# print('comi pra caramba')
+# for cont in range(0,len(lanche)):
+    # print(f'eu vou comer {lanche[cont]} na posição {cont}')
+
+# lanche = ('hamburguer','suco','pizza','pudim')
+# print(sorted (lanche))
+# a = (2,5,4)
+# b = (5,8,1,2)
+# print (a)
+# print (b)
+# c = a+b
+# print (c)
+# print (len (c))
+# print(c.count(5))
+# print(c.index(5))
+# print(c.index(5,2))
+
+pessoa = ('isabella',17,'f',55)
+print(pessoa)
+del pessoa 
+print(pessoa)
