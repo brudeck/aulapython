20efa2fe1f5e8fc9999b7ec54b42e6e4c475fd14
isabella <isabella.brudeck.machado@escola.pr.gov.br>
2026-08-18 08:33:24 -0300

    ..

--- exercicios/atividade68.py
+++ exercicios/atividade68.py
@@ -1,4 +1,3 @@
-# Criação da tupla única com produtos e preços na sequência
 listagem = (
     "Lápis", 1.75,
     "Borracha", 2.00,
@@ -11,17 +10,13 @@ listagem = (
     "Livro", 34.90
 )
 
-# Cabeçalho da tabela
 print("-" * 40)
 print(f'{"LISTAGEM DE PREÇOS":^40}')
 print("-" * 40)
 
-# Estrutura de repetição para tabular os dados
 for posicao in range(0, len(listagem)):
-    # Se a posição for par, trata-se do nome do produto
     if posicao % 2 == 0:
         print(f"{listagem[posicao]:.<30}", end="")
-    # Se a posição for ímpar, trata-se do preço
     else:
         print(f"R$ {listagem[posicao]:>7.2f}")
 
