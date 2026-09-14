a045ab471fcd9fbd776b0fdd9e7b7de031af9d4e
isabella <isabella.brudeck.machado@escola.pr.gov.br>
2026-09-14 09:23:38 -0300

    ..

--- exercicios/conteúdo/cont15.py
+++ exercicios/conteúdo/cont15.py
@@ -14,12 +14,21 @@
 # for p in galera:
 #     print(f'{p[0]} tem {p[1]} anos de idade')
 
-galera = []
-dado = []
-tomai = tomen = 0
-for c in range (0,3):
-    dado.append(str(input('nome')))
-    dado.append(int(input('idade')))
-    galera.append(dado[:])
-    dado.clear()
-print(galera)
+ galera = []
+ dado = []
+ tomai = tomen = 0
+ for c in range (0,3):
+     dado.append(str(input('nome')))
+     dado.append(int(input('idade')))
+     galera.append(dado[:])
+     dado.clear()
+ print(galera)
+
+for p in galera:
+    if p[1] >=18:
+        print(f'(p[0]} é maior de idade')
+        tomai += 1
+    else:
+        print(f'(p[0]) é menor de idade')
+        tomen += 1
+print(f'Temos (tomai) maiores de idade e {tomen) menores de idade)
