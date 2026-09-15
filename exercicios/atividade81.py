891f43af82b04622764ab0274a5634d58c24b68e
isabella <isabella.brudeck.machado@escola.pr.gov.br>
2026-09-15 11:59:14 -0300

    ..

---
+++ exercicios/atividade81.py
@@ -0,0 +1,12 @@
+valor = []
+continuar = 's'
+while continuar == 's':
+    num =int(input('Digite um valor'))
+if num in valor:
+    print('valor duplicado! não vou adicionar')
+else:
+    valor.append(num)
+    print('Valor adicionado com sucesso')
+continuar = str(input('Quer continuar?')).lower().strip()[0]
+valor.sort()
+print(f'Você digitou os valores {valor}')
---
+++ exercicios/atividade86.py
@@ -0,0 +1,28 @@
+temp = []
+princ = []
+mai = men = 0
+while True:
+    temp.append(str(input('Nome:')))
+    temp.append(float(input('Peso:')))
+    if len(princ) == 0: #nenhum cadastro
+        mai = men = temp[1]
+    else:
+        if temp[1]>mai:
+            mai = temp[1]
+        if temp[1]<men:
+            men = temp[1]
+    princ.append(temp[:])
+    temp.clear()
+    resp = str(input('Quer continuar?')).lower().strip()[0]
+    if resp == 'n':
+        break
+print(f'os dados foram {princ}')
+print(f'você cadastrou {len(princ)} pessoas')
+print(f'o maior peso foi de {mai} kg. peso de', end='')
+for p in princ:
+    if p[1] == mai:
+        print(f'[{p[0]}]', end='')
+print(f'o menor peso foi de {men} kg. peso de', end='')
+for p in princ:
+    if p[1] == men:
+        print(f'[{p[0]}]', end='')
